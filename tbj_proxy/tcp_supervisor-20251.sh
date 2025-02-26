4facdc3cc57ffa8c1810dedac1e16c71e522dede
iorichina <iorichina@gmail.com>
2025-02-26 23:36:16 +0800

    fix supervisor

--- tbj_proxy/tcp_supervisor-20251.sh
+++ tbj_proxy/tcp_supervisor-20251.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-go build tcp_proxy.go
+go build tbj_proxy.go
 
 while true; do
-	server=`ps -ef | grep tcp_proxy | grep "192.168.1.101:2102" | grep -v grep`
+	server=`ps -ef | grep tbj_proxy | grep "192.168.1.101:2102" | grep -v grep`
 	if [ ! "$server" ]; then
-		./tcp_proxy 0.0.0.0:20251 3000  8.138.173.89:20009 3000 >>log-20251.log 2>&1 &
+		./tbj_proxy 0.0.0.0:20251 3000  8.138.173.89:20009 3000 >>log-20251.log 2>&1 &
 		sleep 5
 	fi
 	sleep 1
